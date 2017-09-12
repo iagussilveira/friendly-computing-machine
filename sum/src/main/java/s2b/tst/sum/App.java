d8047687edf6fc1aae641d6551dca9a0640f8fb0
teste <teste@10.32.175.101>
2017-09-12 15:13:16 -0300

    #1

--- sum/src/main/java/s2b/tst/sum/App.java
+++ sum/src/main/java/s2b/tst/sum/App.java
@@ -1,13 +1,22 @@
 package s2b.tst.sum;
 
+import java.util.Scanner;
+
 /**
- * Hello world!
+ * Sum!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.printf("A:\n");
+		double a = input.nextDouble();
+		System.out.printf("B:\n");
+		double b = input.nextDouble();
+		input.close();
+		double r = Sum.sum(a, b);
+		System.out.printf("A= %f\n", a);
+		System.out.printf("B= %f\n", b);
+		System.out.printf("SUM= %f\n", r);
+	}
 }
